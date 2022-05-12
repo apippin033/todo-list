8d9f01cd2b28dfd2a97f4a313b76f4e6e107ca00
Alan Pippin <ajpip33@gmail.com>
2022-05-12 12:22:44 -0400

    MUI added

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,19 +1,20 @@
 import React from "react";
+import Card from "@mui/material/Card";
 
 export const TodoItem = ({ todo, toggleComplete, removeTodo }) => {
   return (
     <div>
-      <p>
+      <Card>
         {todo.description}
         {todo.complete ? "Complete" : "Not Complete"}
+        <button onClick={() => removeTodo(todo.id)}>delete</button>
         <input
           type={"checkbox"}
           onChange={() => toggleComplete()}
           name={"complete"}
           id={todo.id}
         />{" "}
-      </p>
-      <button onClick={() => removeTodo(todo.id)}>delete</button>
+      </Card>
     </div>
   );
 };
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { TodoItem } from "./todoItem.js";
 
-export const TodoList = ({ todos, toggleComplete }) => {
+export const TodoList = ({ todos, toggleComplete, removeTodo }) => {
   return todos.map((todo) => {
     return (
-      <TodoItem key={todo.id} todo={todo} toggleComplete={toggleComplete} />
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
+      />
     );
   });
 };
